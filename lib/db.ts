ed7a6ec4ee71576dce94663516bf9ee45deeb134
Shubham Bhardwaj <bhardwajshub0001@gmail.com>
2025-07-30 10:55:56 +0530

    connection setup

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import { buffer } from "stream/consumers";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
     throw new Error(
@@ -22,8 +23,18 @@ export async function connectToDatabase() {
 
     if(!cached.promise){
         const opts ={
-            
+            bufferCommands : true,
+            maxPoolSize: 10, 
         }
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
+
     }
+    try{
+        cached.conn = await cached.promise
+    } catch (error){
+        cached.promise = null;
+    }
+
+    return cached.conn
 
 }
